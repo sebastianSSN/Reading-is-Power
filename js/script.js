b7ee4b4a4a792b27f1661a0a06f92b5f4d8f13b3
sebastian <juansnova@gmail.com>
2021-10-16 23:41:50 -0500

    v0.3

--- js/script.js
+++ js/script.js
@@ -7,26 +7,26 @@ var btn_open = document.getElementById("btn_open");
 var body = document.getElementById("body");
 
 //Evento para mostrar y ocultar menú
-    function open_close_menu(){
-        body.classList.toggle("body_move");
-        side_menu.classList.toggle("menu__side_move");
-    }
+function open_close_menu() {
+    body.classList.toggle("body_move");
+    side_menu.classList.toggle("menu__side_move");
+}
 
 //Si el ancho de la página es menor a 760px, ocultará el menú al recargar la página
 
-if (window.innerWidth < 760){
+if (window.innerWidth < 760) {
     body.classList.add("body_move");
     side_menu.classList.add("menu__side_move");
 }
 
 //Haciendo el menú responsive(adaptable)
 
-window.addEventListener("resize", function(){
-    if (window.innerWidth > 760){
+window.addEventListener("resize", function () {
+    if (window.innerWidth > 760) {
         body.classList.remove("body_move");
         side_menu.classList.remove("menu__side_move");
     }
-    if (window.innerWidth < 760){
+    if (window.innerWidth < 760) {
         body.classList.add("body_move");
         side_menu.classList.add("menu__side_move");
     }
@@ -36,11 +36,11 @@ window.addEventListener("resize", function(){
 
 document.getElementById("button-up").addEventListener("click", scrollUp);
 
-function scrollUp(){
+function scrollUp() {
     var currentScroll = document.documentElement.scrollTop;
-    if (currentScroll > 0){
+    if (currentScroll > 0) {
         window.requestAnimationFrame(scrollUp);
-        window.scrollTo (0, currentScroll - (currentScroll / 10));
+        window.scrollTo(0, currentScroll - (currentScroll / 10));
     }
 }
 
@@ -48,11 +48,11 @@ function scrollUp(){
 
 buttonUp = document.getElementById("button-up");
 
-window.onscroll = function(){
+window.onscroll = function () {
     var scroll = document.documentElement.scrollTop;
-    if (scroll > 500){
+    if (scroll > 500) {
         buttonUp.style.transform = "scale(1)";
-    }else if(scroll < 500){
+    } else if (scroll < 500) {
         buttonUp.style.transform = "scale(0)";
     }
 }
@@ -65,3 +65,43 @@ btnSwitch.addEventListener('click', () => {
     document.body.classList.toggle('dark');
     btnSwitch.classList.toggle('active');
 });
+
+//Buscador de contenido
+
+inputSearch = document.getElementById("inputSearch");
+box_search = document.getElementById("box-search");
+
+//Creando filtrado de busqueda
+
+document.getElementById("inputSearch").addEventListener("keyup", buscador_interno);
+
+function buscador_interno() {
+    filter = inputSearch.value.toUpperCase();
+    li = box_search.getElementsByTagName("li");
+
+    //Recorriendo elementos a filtrar mediante los "li"
+    for (i = 0; i < li.length; i++) {
+
+        a = li[i].getElementsByTagName("a")[0];
+        textValue = a.textContent || a.innerText;
+
+        if (textValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+            box_search.style.display = "block";
+            if (inputSearch.value === "") {
+                box_search.style.display = "none";
+            }
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+// ocultar los resultados de la busqueda
+
+document.getElementById("body").addEventListener("click", ocultar_busqueda);
+
+function ocultar_busqueda() {
+    if (box_search.style.display === "block") {
+        box_search.style.display = "none";
+    }
+}
